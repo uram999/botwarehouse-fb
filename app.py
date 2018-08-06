6fb1f3a2a63b7c0d321791c0750bdf1e37870a93
이우람 <uram999@naver.com>
2018-08-06 16:03:14 +0900

    로그 처리 추가

--- app.py
+++ app.py
@@ -32,16 +32,17 @@ def webhook():
 
     if data["object"] == "page":
 
-        make_botton()
-
         for entry in data["entry"]:
             for messaging_event in entry["messaging"]:
+
+                print(messaging_event)
 
                 if messaging_event.get("message"):  # someone sent us a message
 
                     sender_id = messaging_event["sender"]["id"]        # the facebook ID of the person sending you the message
                     recipient_id = messaging_event["recipient"]["id"]  # the recipient's ID, which should be your page's facebook ID
                     message_text = messaging_event["message"]["text"]  # the message's text
+                    print(message_text)
 
                     send_message(sender_id, "roger that!")
 
