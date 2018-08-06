f7f7b54e29f6a5765d8b7e12269847fc6613dbcd
이우람 <uram999@naver.com>
2018-08-06 17:25:59 +0900

    조회 Generic 정보 Serialize 수정

--- app.py
+++ app.py
@@ -72,7 +72,7 @@ def get_list_info(recipient_id):
 
     data = json.loads(response.text)
     print(data)
-    print(data.values())
+    print(data[0].values())
     generic_info = {
         "attachment": {
             "type": "template",
