155f75116e6b1c5f9214e09ce1bca0bbcad242cc
이우람 <uram999@naver.com>
2018-08-28 13:06:43 +0900

    제네릭 호출 테스트

--- app.py
+++ app.py
@@ -207,9 +207,9 @@ def send_generic(recipient_id, generic_info):
 
     r = requests.post("https://graph.facebook.com/v2.6/me/messages", params=params, headers=headers, data=data)
 
-    print(r)
     if r.status_code != 200:
-        log(r.status_code)
+        # log(r.status_code)
+        print(r.status_code)
         log(r.text)
 
 
