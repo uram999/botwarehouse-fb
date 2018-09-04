6737e0afa0ebb29625daf952754c9d1d40752d95
이우람 <uram999@naver.com>
2018-09-04 17:37:13 +0900

    로그 처리

--- app.py
+++ app.py
@@ -223,6 +223,7 @@ def stock_modify_update(recipient_id, user_id, payload_data):
         .format(user_id=user_id, pre_code=payload_data[2], new_code=payload_data[3])
     response = requests.get(api_url)
 
+    print(response.text)
     data = json.loads(response.text)
 
     send_message(recipient_id, "관심 종목이 수정되었습니다.")
